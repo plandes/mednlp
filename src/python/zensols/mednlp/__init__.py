9b6037193c799abc23820e99b14c523015557d8f
Paul Landes <landes@mailc.net>
2023-06-09 12:40:57 -0500

    rename module

--- src/python/zensols/mednlp/__init__.py
+++ src/python/zensols/mednlp/__init__.py
@@ -1,7 +1,7 @@
 from .domain import *
 from .uts import UTSError, NoResultsError, AuthenticationError, UTSClient
 from .resource import *
-from .token import *
+from .tok import *
 from .lib import *
 from .parser import *
 from .app import *
