d0205addd39347ac7885c5670f2c874d2f9e7fb4
Paul Landes <landes@mailc.net>
2024-02-10 17:41:21 -0600

    add scispacy biomedical parser config

--- src/python/zensols/mednlp/parser.py
+++ src/python/zensols/mednlp/parser.py
@@ -6,6 +6,7 @@ __author__ = 'Paul Landes'
 from typing import Type, Iterable, Dict, Set
 from dataclasses import dataclass, field
 import logging
+import warnings
 import collections
 from spacy.tokens.doc import Doc
 from spacy.language import Language
@@ -44,6 +45,11 @@ class MedicalFeatureDocumentParser(SpacyFeatureDocumentParser):
         if self.medcat_resource is None:
             raise MedNLPError('No medcat resource set')
         super().__post_init__()
+        # filter spacy language warning when using scispacy
+        warnings.filterwarnings(
+            'ignore',
+            message=r'^Possible set union at position',
+            category=FutureWarning)
 
     def _create_model_key(self) -> str:
         return f'name-{self.name}'
